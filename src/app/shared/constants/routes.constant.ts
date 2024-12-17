0dda4fa2b055b9e209e0bcd40c55fcce4fa5c488
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2024-12-16 21:35:32 -0300

    feat: se agrega el tema con los colores, fuentes y se agregan las rutas

---
+++ src/app/shared/constants/routes.constant.ts
@@ -0,0 +1,12 @@
+/**
+ * A collection of route paths.
+ */
+export const routePaths = {
+  auth: 'auth',
+  dashboard: 'dashboard',
+  login: 'login',
+  notFound: '**',
+  pushSend: 'envio-push',
+  root: '',
+  smsSend: 'envio-sms'
+}
