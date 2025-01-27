79de80a73c9f90ef5a3a8849843ea3a06c0d499a
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-26 23:19:06 -0300

    feat: code

--- src/app/features/push-send/push-send.page.component.ts
+++ src/app/features/push-send/push-send.page.component.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap'
+
+import { PushSendIndividualContainerComponent } from './ui/containers/push-send-individual/push-send-individual.container.component'
+import { PushSendMassiveContainerComponent } from './ui/containers/push-send-massive/push-send-massive.container.component'
 
 /**
  * Push send page.
  */
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [NgbNavModule, PushSendIndividualContainerComponent, PushSendMassiveContainerComponent],
   selector: 'app-push-send-page',
   styleUrl: './push-send.page.component.scss',
   templateUrl: './push-send.page.component.html'
 })
-export class PushSendPageComponent {}
+export class PushSendPageComponent {
+  activeTab = 'individual'
+}
